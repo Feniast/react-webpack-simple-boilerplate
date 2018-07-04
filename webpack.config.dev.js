48d603e8b0d3f26ad264e7130a1f9085f1b05faf
tang-chen-yang <tang-chen-yang@hpe.com>
2018-07-04 18:16:51 +0800

    init

---
+++ webpack.config.dev.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash:8].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: 'babel-loader' 
+      },
+
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    publicPath: '/',
+    historyApiFallback: false,
+    contentBase: './public',
+    compress: true,
+    watchContentBase: true,
+    inline: true,
+    hot: true,
+    port: 8080,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+  devtool: 'cheap-module-source-map',
+  mode: 'development'
+}
