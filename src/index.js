45eea2b8d5a6e66f43d5b27e2d2f27f1a19c1007
tang-chen-yang <tang-chen-yang@hpe.com>
2018-07-10 11:08:21 +0800

    change babel settings to support async/await

--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import logo from './assets/logo.svg';
 
-const App = () => (
-  <div>
-    <h1>Hello React</h1>
-    <img src={logo} alt="react" />
-  </div>
-);
+const hello = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => resolve(), 1000);
+  });
+};
+
+class App extends React.Component {
+  state = {
+    message: 'Hello React'
+  }
+
+  componentDidMount() {
+    hello().then(() => this.setState({
+      message: 'Hello World'
+    }));
+  }
+
+  render() {
+    const { message } = this.state;
+    return (
+      <div>
+        <h1>{message}</h1>
+        <img src={logo} alt="react" />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
